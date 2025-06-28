7bfd99066a27f0de1b9d7dd88fc7048c38347edd
Shiva Teja Konduri <konduri.shiva1996@gmail.com>
2025-06-28 17:01:03 +0530

    test script update
    test script update

--- backend/input_handler/input_handler_test.py
+++ backend/input_handler/input_handler_test.py
@@ -44,16 +44,16 @@ cursor.execute("""
 input_csv = pd.read_csv(io.BytesIO(input_csv_object['Body'].read()))
 for index, row in input_csv.iterrows():
     print(f"Processing row {index}: {row}")
-    articles_id = str(uuid.uuid4())  # Generate a unique ID for each article
+    article_id = str(uuid.uuid4())  # Generate a unique ID for each article
     cursor.execute("""
-            INSERT INTO articles (articles_id, title, body, source, published_date)
-            VALUES (%s, %s, %s, %s, %s)""", (articles_id, row[1], row[2], row[3], row[4]))
+            INSERT INTO articles (article_id, title, body, source, published_date)
+            VALUES (%s, %s, %s, %s, %s)""", (article_id, row[1], row[2], row[3], row[4]))
     # Convert to CSV in-memory
     csv_buffer = io.StringIO()
     writer = csv.writer(csv_buffer)
     # writer.writerow(row.headers)  # Write header
     writer.writerow(row)
-    s3_path = 'input/' + articles_id + '.csv'
+    s3_path = 'input/' + article_id + '.csv'
     s3_uri = 's3://' + bucket_name + '/' + s3_path
     s3.put_object(
         Bucket=bucket_name,
@@ -66,7 +66,7 @@ for index, row in input_csv.iterrows():
         OutputDataConfig={'S3Uri': 's3://awstraindata/output/entities/'},
         DataAccessRoleArn=role_arn,
         LanguageCode='en',
-        JobName='MyEntityDetectionJob_'+ articles_id + '_' + str(int(time.time()))
+        JobName='MyEntityDetectionJob_'+ article_id + '_' + str(int(time.time()))
     )
     result = comprehend.describe_entities_detection_job(JobId=entities_job['JobId'])
     entities_output = result['EntitiesDetectionJobProperties']['OutputDataConfig']['S3Uri']
@@ -77,7 +77,7 @@ for index, row in input_csv.iterrows():
         OutputDataConfig={'S3Uri': 's3://awstraindata/output/sentiment/'},
         DataAccessRoleArn=role_arn,
         LanguageCode='en',
-        JobName='MySentimentDetectionJob_' + articles_id + '_' + str(int(time.time()))
+        JobName='MySentimentDetectionJob_' + article_id + '_' + str(int(time.time()))
     )
     res = comprehend.describe_sentiment_detection_job(JobId=sentiment_job['JobId'])
     sentiment_output = res['SentimentDetectionJobProperties']['OutputDataConfig']['S3Uri']
@@ -88,7 +88,7 @@ for index, row in input_csv.iterrows():
         OutputDataConfig={'S3Uri': 's3://awstraindata/output/keyphrases/'},
         DataAccessRoleArn=role_arn,
         LanguageCode='en',
-        JobName='MyKeyPhrasesDetectionJob_' + articles_id + '_' + str(int(time.time()))
+        JobName='MyKeyPhrasesDetectionJob_' + article_id + '_' + str(int(time.time()))
     )
     res = comprehend.describe_key_phrases_detection_job(JobId=phrases_job['JobId'])
     key_phrases_output = res['KeyPhrasesDetectionJobProperties']['OutputDataConfig']['S3Uri']
@@ -98,7 +98,7 @@ for index, row in input_csv.iterrows():
     print("Inserting into comprehend_jobs table")
     cursor.execute("""
         INSERT INTO comprehend_jobs (article_id, input_s3_uri, entities_path, sentiment_path, key_phrases_path)
-        VALUES (%s, %s, %s, %s, %s)""", (articles_id, s3_uri, entities_output.replace('s3://awstraindata/', ''), sentiment_output.replace('s3://awstraindata/', ''), key_phrases_output.replace('s3://awstraindata/', '')))
+        VALUES (%s, %s, %s, %s, %s)""", (article_id, s3_uri, entities_output.replace('s3://awstraindata/', ''), sentiment_output.replace('s3://awstraindata/', ''), key_phrases_output.replace('s3://awstraindata/', '')))
     conn.commit()
 cursor.close()
 conn.close()

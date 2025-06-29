53979e3ecbfec3d4512cf6ae4caccd6410038dc4
Shiva Teja Konduri <konduri.shiva1996@gmail.com>
2025-06-29 06:02:07 +0530

    Create feed.py
    Create feed.py

---
+++ backend/get_feed/get_feed.py
@@ -0,0 +1,20 @@
+
+from Utils import get_postgresql_connection
+
+
+def lambda_handler(event, context):
+    conn = get_postgresql_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM articles order by article_id asc"
+    cursor.execute(query)
+    articles = cursor.fetchall()
+    return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "status": "success",
+                "data": articles
+            })
+    }
